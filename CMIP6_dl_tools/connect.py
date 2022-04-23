a1b5872aa859acc5cdd8023cf87fc28009a0afcc
Anthony Schrapffer <schrapff.ant@gmail.com>
2022-04-23 14:16:04 +0200

    Update connect.py

--- CMIP6_dl_tools/connect.py
+++ CMIP6_dl_tools/connect.py
@@ -3,7 +3,21 @@ from pyesgf.logon import LogonManager
 import os
 
 
-def init_connection(openid,password):
+def init_connection(str openid,str password):
+    """
+    Initialize connection with ESGF Server.
+    
+    Parameters:
+    -----------
+    openid (str): id of the user
+    password (str): password of the user
+    
+    Returns:
+    --------
+    lm (LogonManager object)
+    conn (Connection object)
+    ----------------------------------------------------
+    """
     lm = LogonManager()
     lm.logoff()
     lm.logon_with_openid(openid, password = password, bootstrap = True)
