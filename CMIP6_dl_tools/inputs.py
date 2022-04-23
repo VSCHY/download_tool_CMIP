ee46e942b331698983ed3ae7b1fc839a3e8851cd
Anthony Schrapffer <schrapff.ant@gmail.com>
2022-04-23 14:22:23 +0200

    Update inputs.py : documentation.

--- CMIP6_dl_tools/inputs.py
+++ CMIP6_dl_tools/inputs.py
@@ -1,7 +1,28 @@
-import configparser
-# json output file
-def load_inputs(output_dir = True):
+"""
+Load the .def files with ConfigParser
+"""
 
+import configparser
+
+
+def load_inputs():
+    """
+    Load the parameters for the download in the search.def file.
+    
+    Parameters:
+    -----------
+    None
+    
+    Returns:
+    --------
+    dfile (str): directory of the json file with the model/member
+        with the corresponding experiments/variables available.
+    experiments (list): list of experiment to download.
+    variables (list): list of variables to download.
+    wrkdir (str): directory where the files will be downloaded.
+    
+    ----------------------------------------------------
+    """
     search=configparser.ConfigParser()
     search.read("search.def")
     dfile=search.get("OverAll", "dfile", fallback="./Models_CMIP6.json")
@@ -19,6 +40,20 @@ def load_inputs(output_dir = True):
     return dfile, experiments, variables, wrkdir
 
 def load_login():
+    """
+    Load the user id and password from the config.def file.
+    
+    Parameters:
+    -----------
+    None
+    
+    Returns:
+    --------
+    openid (str): user id.
+    password (str)
+    
+    ----------------------------------------------------
+    """
     config=configparser.ConfigParser()
     config.read("config.def")
     openid=config.get("OverAll", "openid")
